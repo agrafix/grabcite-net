bfb6fe5665ec43696b53c34a696d3cf6d063790d
Alexander Thiemann <mail@athiemann.net>
2017-06-09 15:33:14 +0200

    first version

---
+++ net.py
@@ -0,0 +1,72 @@
+""" Ported from tflearn NLP CNN example
+"""
+
+import tensorflow as tf
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_1d, global_max_pool
+from tflearn.layers.merge_ops import merge
+from tflearn.layers.estimator import regression
+from tflearn.data_utils import to_categorical, pad_sequences
+import numpy
+
+from load_data import load_data
+
+# IMDB Dataset loading
+train, test, check, word_dict_rev = load_data(path='ref_bool.pkl', n_words=10000,
+                                              valid_portion=0.1)
+
+trainX, trainY = train
+testX, testY = test
+checkX, checkY = check
+
+print("Train size: " + str(len(trainX)))
+print("Test size: " + str(len(testX)))
+print("Check size: " + str(len(checkX)))
+
+print(testX[1:5])
+print(testY[1:5])
+
+# Data preprocessing
+# Sequence padding
+trainX = pad_sequences(trainX, maxlen=100, value=0.)
+testX = pad_sequences(testX, maxlen=100, value=0.)
+checkX = pad_sequences(checkX, maxlen=100, value=0.)
+
+print(testX[1:5])
+print(testY[1:5])
+
+# Converting labels to binary vectors
+trainY = to_categorical(trainY, nb_classes=2)
+testY = to_categorical(testY, nb_classes=2)
+checkY = to_categorical(checkY, nb_classes=2)
+
+print(testX[1:5])
+print(testY[1:5])
+
+# Building convolutional network
+network = input_data(shape=[None, 100], name='input')
+network = tflearn.embedding(network, input_dim=10000, output_dim=128)
+branch1 = conv_1d(network, 128, 3, padding='valid', activation='relu', regularizer="L2")
+branch2 = conv_1d(network, 128, 4, padding='valid', activation='relu', regularizer="L2")
+branch3 = conv_1d(network, 128, 5, padding='valid', activation='relu', regularizer="L2")
+network = merge([branch1, branch2, branch3], mode='concat', axis=1)
+network = tf.expand_dims(network, 2)
+network = global_max_pool(network)
+network = dropout(network, 0.5)
+network = fully_connected(network, 2, activation='softmax')
+network = regression(network, optimizer='adam', learning_rate=0.001,
+                     loss='categorical_crossentropy', name='target')
+# Training
+model = tflearn.DNN(network, tensorboard_verbose=0)
+model.fit(trainX, trainY, n_epoch = 5, shuffle=True, validation_set=(testX, testY), show_metric=True, batch_size=32)
+
+print(checkX[1:5])
+results = model.predict(checkX)
+for idx, val in enumerate(results):
+    sentence_idx = checkX[idx]
+    sentence = [word_dict_rev[i] if i in word_dict_rev else '<unk>' for i in sentence_idx]
+
+    if val[0] < val[1]:
+        print(sentence)
+        print("Citation: " + str(val[1]))
---
+++ prepare_data.py
@@ -0,0 +1,75 @@
+import glob
+import os
+import re
+import nltk
+import six.moves.cPickle as pickle
+
+refRegex = re.compile(r"<(DBLP|ARXIV|DOI|GC):([^>]+)>")
+
+def prepare_sentence(sentence):
+    refs = refRegex.findall(sentence)
+    sentence = refRegex.sub("", sentence)
+    toks = [w.lower() for w in nltk.word_tokenize(sentence) if w.isalpha()]
+    return refs, toks
+
+def mk_output(dataSet):
+    outputFormat = [[], []]
+    for x, y in dataSet:
+        outputFormat[0].append(x)
+        outputFormat[1].append(0 if len(y) == 0 else 1)
+    return outputFormat
+
+def build_dataset(targetFile):
+    refDict = {}
+    refDictCtr = 1
+    refDictRev = {}
+
+    wordDict = {}
+    wordDictCtr = 2 # start at 2, 1 means <unk> word
+    wordDictRev = {}
+
+    trainSet = []
+    testSet = []
+
+    idx = 0
+    for file in glob.glob("data/scholarly-cit-sentences/*.txt"):
+        with open(file, 'r') as myfile:
+            data = myfile.read().split("\n============\n")
+            for sentence in data:
+                refs, toks = prepare_sentence(sentence)
+
+                Y = []
+                for (refType, refId) in refs:
+                    key = refType + ":" + refId
+                    if key not in refDict:
+                        refDict[key] = refDictCtr
+                        refDictRev[refDictCtr] = key
+                        refDictCtr += 1
+                    Y.append(refDict[key])
+
+                X = []
+                for tok in toks:
+                    if tok not in wordDict:
+                        wordDict[tok] = wordDictCtr
+                        wordDictRev[wordDictCtr] = tok
+                        wordDictCtr += 1
+                    X.append(wordDict[tok])
+
+                if idx % 10 == 0:
+                    testSet.append((X, Y))
+                else:
+                    trainSet.append((X, Y))
+                idx += 1
+
+    trainData = mk_output(trainSet)
+    testData = mk_output(testSet)
+
+    print("Train data size: " + str(len(trainData[0])))
+    print("Test data size: " + str(len(testData[1])))
+
+    with open(targetFile, 'wb') as f:
+        pickle.dump(trainData, f)
+        pickle.dump(testData, f)
+        pickle.dump(wordDictRev, f)
+
+build_dataset("ref_bool.pkl")
