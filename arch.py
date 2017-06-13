f7f8dccefafbdd8d8393ed04c97e665452dc08e2
Alexander Thiemann <mail@athiemann.net>
2017-06-13 11:05:34 +0200

    extract network arch in extra file

---
+++ arch.py
@@ -0,0 +1,26 @@
+import tflearn
+import tensorflow as tf
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_1d, global_max_pool
+from tflearn.layers.merge_ops import merge
+from tflearn.layers.estimator import regression
+
+def cit_nocit_cnn(max_sentence_len, max_words):
+
+    # Building convolutional network
+    network = input_data(shape=[None, max_sentence_len], name='input')
+    network = tflearn.embedding(network, input_dim=max_words, output_dim=128)
+    branch1 = conv_1d(network, 128, 3, padding='valid', activation='relu', regularizer="L2")
+    branch2 = conv_1d(network, 128, 4, padding='valid', activation='relu', regularizer="L2")
+    branch3 = conv_1d(network, 128, 5, padding='valid', activation='relu', regularizer="L2")
+    network = merge([branch1, branch2, branch3], mode='concat', axis=1)
+    network = tf.expand_dims(network, 2)
+    network = global_max_pool(network)
+    network = dropout(network, 0.5)
+    network = fully_connected(network, 2, activation='softmax')
+    network = regression(network, optimizer='adam', learning_rate=0.001,
+                         loss='categorical_crossentropy', name='target')
+    # Training
+    model = tflearn.DNN(network, tensorboard_verbose=1)
+
+    return model
--- net.py
+++ net.py
@@ -3,30 +3,27 @@
 
 import tensorflow as tf
 import tflearn
-from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.layers.conv import conv_1d, global_max_pool
-from tflearn.layers.merge_ops import merge
-from tflearn.layers.estimator import regression
 from tflearn.data_utils import to_categorical, pad_sequences
 import numpy
 
 from load_data import load_data
+from arch import cit_nocit_cnn
 
 # configuration
 max_words = 10000
 max_sentence_len = 50
 
-# IMDB Dataset loading
-train, test, check, word_dict_rev = load_data(path='ref_bool.pkl', n_words=max_words,
-                                              valid_portion=0.1)
+# Dataset loading
+
+print("Loading dataset ... ")
+train, test, _, _ = load_data(path='ref_bool.pkl', n_words=max_words,
+                              valid_portion=0.1)
 
 trainX, trainY = train
 testX, testY = test
-checkX, checkY = check
 
 print("Train size: " + str(len(trainX)))
 print("Test size: " + str(len(testX)))
-print("Check size: " + str(len(checkX)))
 
 print(testX[1:5])
 print(testY[1:5])
@@ -35,7 +32,6 @@ print(testY[1:5])
 # Sequence padding
 trainX = pad_sequences(trainX, maxlen=max_sentence_len, value=0.)
 testX = pad_sequences(testX, maxlen=max_sentence_len, value=0.)
-checkX = pad_sequences(checkX, maxlen=max_sentence_len, value=0.)
 
 print(testX[1:5])
 print(testY[1:5])
@@ -43,38 +39,12 @@ print(testY[1:5])
 # Converting labels to binary vectors
 trainY = to_categorical(trainY, nb_classes=2)
 testY = to_categorical(testY, nb_classes=2)
-checkY = to_categorical(checkY, nb_classes=2)
 
 print(testX[1:5])
 print(testY[1:5])
 
-# Building convolutional network
-network = input_data(shape=[None, max_sentence_len], name='input')
-network = tflearn.embedding(network, input_dim=max_words, output_dim=128)
-branch1 = conv_1d(network, 128, 3, padding='valid', activation='relu', regularizer="L2")
-branch2 = conv_1d(network, 128, 4, padding='valid', activation='relu', regularizer="L2")
-branch3 = conv_1d(network, 128, 5, padding='valid', activation='relu', regularizer="L2")
-network = merge([branch1, branch2, branch3], mode='concat', axis=1)
-network = tf.expand_dims(network, 2)
-network = global_max_pool(network)
-network = dropout(network, 0.5)
-network = fully_connected(network, 2, activation='softmax')
-network = regression(network, optimizer='adam', learning_rate=0.001,
-                     loss='categorical_crossentropy', name='target')
-# Training
-model = tflearn.DNN(network, tensorboard_verbose=0)
+# Train
+model = cit_nocit_cnn(max_sentence_len, max_words)
 model.fit(trainX, trainY, n_epoch = 10, shuffle=True, validation_set=(testX, testY), show_metric=True, batch_size=32)
 model.save("trained_model.tfl")
 print("Wrote model to trained_model.tfl")
-
-results = model.predict(checkX)
-
-with open("train_results.txt", 'w') as f:
-    for idx, val in enumerate(results):
-        sentence_idx = checkX[idx]
-        sentence = [word_dict_rev[i] if i in word_dict_rev else '<unk>' for i in sentence_idx]
-
-        if val[0] < val[1]:
-            f.write("[CIT: " + str(round(val[1] * 100, 2)) + "]" + " ".join(sentence) + "\n")
-        else:
-            f.write("[NO CIT: " + str(round(val[0] * 100, 2)) + "]" + " ".join(sentence) + "\n")
