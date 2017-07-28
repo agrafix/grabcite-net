5e0d1bd6afc748a01b269019464cfe1939218a3d
Alexander Thiemann <mail@athiemann.net>
2017-07-28 16:03:02 +0200

    update visualisation tool to generate citation graph and other improvs

--- vis.py
+++ vis.py
@@ -2,15 +2,12 @@
 
 # local imports
 import prepare_data as pd
-import seq2seq_data as s2s
-import dblp as dblp
 
 # package imports
 import glob
 import numpy as np
 from tqdm import tqdm
-import six.moves.cPickle as pickle
-from lxml import etree
+from graphviz import Digraph
 
 import matplotlib
 matplotlib.use('agg')
@@ -19,19 +16,16 @@ plt.style.use('ggplot')
 
 # std lib
 from collections import Counter
-from xml.dom import minidom
 from urllib.request import urlopen
 import os
 import time
 import sys
+import csv
+from functools import lru_cache
+import urllib.parse
+import json
 
 data_glob = "data-tiny/*.txt"
-
-if not os.path.exists("dblp.db"):
-    print("Creating a DBLP lookup map... ")
-    dblp.go('dblp.xml', 'dblp.db')
-
-dblpMap = dblp.openMap('dblp.db')
 
 citMap = []
 citTyMap = []
@@ -48,36 +42,74 @@ def countEntries(d, entries):
     for e in entries:
         countUp(d, e)
 
+def writeCsv(counter, csvName):
+    print("Writing " + csvName)
+    with open(csvName, 'w') as csvfile:
+        cwriter = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for (k, v) in counter.most_common():
+            cwriter.writerow([k, v])
+
 def plotHist(d, name, fname):
     counter = Counter(d)
     cats = []
     counts = []
-    for (k, v) in counter.most_common():
+    for (k, v) in tqdm(counter.most_common(), desc='Bulding histogram data', unit='dp'):
         cats.append(k)
         counts.append(v)
 
-    indexes = np.arange(len(cats))
-    width = 0.7
+    if len(cats) < 5000:
+        indexes = np.arange(len(cats))
+        width = 0.7
 
-    plt.figure(figsize=(40, 10))
-    plt.gcf().subplots_adjust(bottom=0.25)
+        plt.figure(figsize=(40, 10))
+        plt.gcf().subplots_adjust(bottom=0.25)
 
-    plt.suptitle(name, fontsize=14, fontweight='bold')
-    plt.bar(indexes, counts, width)
-    plt.xticks(indexes + width * 0.5, cats, rotation='vertical', fontsize=6)
-    # plt.tight_layout()  <-- this breaks atm
+        plt.suptitle(name, fontsize=14, fontweight='bold')
+        plt.bar(indexes, counts, width)
+        plt.xticks(indexes + width * 0.5, cats, rotation='vertical', fontsize=6)
+        # plt.tight_layout()  <-- this breaks atm
 
-    print("Writing " + fname)
-    plt.savefig(fname, dpi = (200))
-    plt.close()
+        print("Writing " + fname)
+        plt.savefig(fname, dpi = (200))
+        plt.close()
+    else:
+        print("Too many entries, skipping generation of " + fname)
+
+    csvName = os.path.splitext(fname)[0] + ".csv"
+    writeCsv(counter, csvName)
+
+@lru_cache(maxsize=10000)
+def getEntry(ref):
+    recId = ref.replace("http://dblp.org/rec/", "")
+    q = urllib.parse.urlencode({ "id": recId })
+    url = "http://papergrep.com/api/get?" + q
+    print("Fetching " + url)
+
+    try:
+        output = urlopen(url).read()
+        entry = json.loads(output)
+        return entry
+    except:
+        print(sys.exc_info())
+        return None
 
 if not os.path.exists("vis"):
     os.makedirs("vis")
 
 countedPapers = set()
 
+graphMap = {}
+
 print("Working on " + data_glob)
 for file in glob.glob(data_glob):
+    metaName = os.path.splitext(file)[0] + ".meta"
+    myUrl = None
+    if os.path.exists(metaName):
+        with open(metaName, 'r') as metaHandle:
+            data = json.loads(metaHandle.read())
+            myUrl = data["info"]["url"]
+    print("DBLP of " + file + " is " + str(myUrl))
+
     with open(file, 'r') as myfile:
         data = myfile.read().split("\n============\n")
         for sentence in data:
@@ -94,23 +126,38 @@ for file in glob.glob(data_glob):
                 countUp(citTyMap, ty)
 
                 if ty == 'DBLP':
-                    dblpId = ref.replace("http://dblp.org/rec/", "")
-                    print("Handling " + dblpId)
+                    if myUrl is not None:
+                        if myUrl not in graphMap:
+                            graphMap[myUrl] = set()
+                        graphMap[myUrl].add(ref)
 
-                    try:
-                        res = dblpMap.Get(dblpId.encode())
-                        entry = pickle.loads(res)
+                    entry = getEntry(ref)
+                    if entry != None:
                         countEntries(authorMap, entry["authors"])
                         countEntries(journalMap, [entry["journal"]])
-                        countEntries(yearMap, [entry["year"]])
-                    except:
-                        print("Unknown dblp id: " + dblpId)
-                        print(sys.exc_info()[0])
+                        countEntries(yearMap, [str(entry["year"])])
+    break # TODO FIXME
 
-
-plotHist(citMap, "Sentences with Citation", "vis/cit_yn.png")
+# Plot all the things
+plotHist(hasCitMap, "Sentences with Citation", "vis/cit_yn.png")
 plotHist(citMap, "Citations", "vis/cits.png")
 plotHist(citTyMap, "Citation Types", "vis/cit_ty.png")
 plotHist(authorMap, "Authors", "vis/authors.png")
 plotHist(journalMap, "Journals", "vis/journals.png")
 plotHist(yearMap, "Years", "vis/years.png")
+
+# Paint the graph
+print("Building the full graph")
+dot = Digraph(comment='Citation Graph')
+allNodes = set(graphMap.keys())
+for k in tqdm(graphMap.keys(), desc='Computing all nodes', unit='sources'):
+    allNodes = allNodes.intersection(graphMap[k])
+
+for n in tqdm(allNodes, desc='Adding all nodes', unit='nodes'):
+    dot.node(n, n)
+
+for k in graphMap.keys():
+    for tgt in graphMap[k]:
+        dot.edge(k, tgt)
+
+dot.render('vis/graph.gv')
